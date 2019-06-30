5f248dd258a6c29cc00d279a1407082c83fca898
vednoc <vednoc@protonmail.com>
2019-06-30 23:54:19 +0200

    core: add basic UI configuration

--- config.el
+++ config.el
@@ -1,2 +1,15 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
+(global-visual-line-mode t)
+(global-git-gutter-mode t)
+
+(setq
+  display-line-numbers-type nil
+
+  git-gutter:update-interval 2
+  git-gutter:window-width -1
+
+
+  doom-theme 'doom-sourcerer
+  doom-font (font-spec :family "Iosevka" :size 14)
+  doom-big-font (font-spec :family "Iosevka" :size 20))
