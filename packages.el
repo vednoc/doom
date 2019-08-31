71a52c64ecbe057f3b4010a188e24e0989f6ab52
vednoc <vednoc@protonmail.com>
2019-08-31 14:43:40 +0200

    lang/stylus: fix stylus-mode missing sws-mode

--- packages.el
+++ packages.el
@@ -5,3 +5,6 @@
 ;; (package! some-package)
 ;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
+
+;;; Fix stylus-mode missing sws-mode.
+(package! sws-mode :recipe (:host github :repo "brianc/jade-mode"))
